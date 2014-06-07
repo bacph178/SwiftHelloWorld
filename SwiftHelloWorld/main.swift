84c553a175919875e12bd66e12d5181b50f3e6bb
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2014-06-07 15:20:48 +0700

    Initial Commit

---
+++ SwiftHelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftHelloWorld
+//
+//  Created by BacPH on 2014/06/07.
+//  Copyright (c) 2014年 Phung.Hoang.Bac. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
