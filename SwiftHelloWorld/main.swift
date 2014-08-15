8c5f488ad918b10cdbc134cec79686a02733aab8
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2014-08-15 15:50:40 +0700

    IF - ELSE

--- SwiftHelloWorld/main.swift
+++ SwiftHelloWorld/main.swift
@@ -7,6 +7,39 @@
 //
 
 import Foundation
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruits"
+println(fruitSummary)
+var shoppingList = ["catfish", "water"]
+println(shoppingList[1])
+shoppingList[1] = "bottle of water"
+println(shoppingList[1])
+var occupations = ["Malcolm":"Captain", "Kaylee":"Mechanic"]
+println(occupations["Malcolm"])
+println(occupations["Kaylee"])
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
 
-println("Hello, World!")
-
+println(teamScore)
+var optionalString: String? = nil
+if optionalString == nil {
+    println("Ok")
+} else {
+    println("not OK")
+}
+var greeting = "Hello"
+if let name = optionalString {
+    greeting = "Hello, \(name)"
+} else {
+    greeting = "Bac dep trai"
+}
+println(greeting)
